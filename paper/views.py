afabe5941eabe6076d54f80696b9ad128dcde2c8
wenheping <wenheping2000@hotmail.com>
2019-12-11 16:26:49 +0800

    simple update

--- paper/views.py
+++ paper/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, Here is paper manage system ")
+
+def display(request, paper_id):
+    res= "Hi, I am paper%s "
+    return HttpResponse(res % paper_id)
