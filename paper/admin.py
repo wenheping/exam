53f00b3966fd9579fd4d5f89817b30ab12e02642
wenheping <wenheping2000@Hotmail.com>
2020-02-25 21:53:38 +0800

    Add database support.

--- paper/admin.py
+++ paper/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Paper
-admin.site.register(Paper)
+from .models import ExamBank
+admin.site.register(ExamBank)
--- paper/models.py
+++ paper/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
-class Paper(models.Model):
-    MyID=models.IntegerField()
+class ExamBank(models.Model):
+  exam_id=models.IntegerField
+  exam_type=models.CharField(max_length=10)
+  exam_loc=models.CharField(max_length=10)
+  exam_time=models.CharField(max_length=10)
+  exam_grade=models.CharField(max_length=20)
+  exam_name=models.CharField(max_length=100)
+  exam_file=models.CharField(max_length=10)
