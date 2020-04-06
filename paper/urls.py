4b297451c45071d3f0aa91c3978f30b96f03d606
wenheping <wenheping2000@hotmail.com>
2020-04-06 22:23:17 +0800

    - Now it work with form

--- paper/urls.py
+++ paper/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:paper_id>/', views.display, name='display'),
+    re_path(r'^index$', views.index, name='index'),
+    re_path(r'^search$', views.search_get, name='search_get'),
+    re_path(r'^ppp', views.index, name='index'),
 ]
--- paper/views.py
+++ paper/views.py
@@ -4,13 +4,35 @@ from django.http import FileResponse, Http404
 
 import os
 
+pdf_dir='/root/wen/exam/wenfiles/pdf'
+
 def index(request):
-    return HttpResponse("Hello, Here is paper manage system ")
+    pdf_name='start.pdf'
+
+    context={}
+    context['file_name']=pdf_name
+    context['what']='File existis !'+pdf_name
+
+    return render(request,'home.html',context)
+
+def search_get(request):
+    if 'q' in request.GET:
+      pdf_name='paper'+request.GET['q']+'.pdf'
+      pdf_file=os.path.join(pdf_dir,pdf_name)
+
+      if os.path.exists(pdf_file)==False:
+         pdf_name='notfound.pdf'
+
+      context={}
+      context['file_name']=pdf_name
+      context['what']='File existis !'+pdf_name
+
+    return render(request,'home.html',context)
 
 def display(request,paper_id):
-    pdf_name="paper"+str(paper_id)+".pdf"
-    pdf_dir="/root/wen/exam/wenfiles/pdf/"
-    pdf_file=pdf_dir+pdf_name
+    pdf_name='paper'+str(paper_id)+'.pdf'
+#    pdf_dir='/root/wen/exam/wenfiles/pdf'
+    pdf_file=os.path.join(pdf_dir,pdf_name)
 
     context={}
 
