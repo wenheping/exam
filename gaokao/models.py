752be1d31c4ef22601865bd54045c6582e7e1a77
wenheping <wenheping2000@hotmail.com>
2020-05-10 20:00:13 +0800

    Add databases for gaokao

--- gaokao/models.py
+++ gaokao/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class GaokaoBank(models.Model):
+  gk_id=models.IntegerField
+  gk_subject=models.CharField(max_length=10)
+  gk_type=models.CharField(max_length=20)
+  gk_year=models.CharField(max_length=20)
+  gk_name=models.CharField(max_length=100)
+  gk_file=models.CharField(max_length=20)
+  gk_file_ans=models.CharField(max_length=5,default='yes')
+  gk_file_rev=models.CharField(max_length=5,default='no')
