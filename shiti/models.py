bb1618759f2416f85e9c1d95ba05df4e812048bd
wen <wen@ob68.wenjing.com>
2020-11-16 19:10:07 +0800

    Add the database for shiti

--- shiti/models.py
+++ shiti/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Content(models.Model):
+    w_id = models.IntegerField(default=0)
+    w_timu = models.CharField(max_length=20)
+    w_daan = models.CharField(max_length=20)
+    w_jiexi_lei = models.CharField(max_length=20)
+    w_jiexi = models.CharField(max_length=20)
+
+class Attrib(models.Model):
+    w_id = models.IntegerField(default=0)
+    w_shijuan_lei = models.CharField(max_length=20)
+    w_shiti_lei = models.CharField(max_length=20)
+    w_quyu = models.CharField(max_length=20)
+    w_nian = models.CharField(max_length=10)
+    w_nandu = models.CharField(max_length=10)
+    w_zhiliang = models.CharField(max_length=10)
+    w_zhishidian = models.CharField(max_length=10)
+    w_guanjianci = models.CharField(max_length=10)
+    
