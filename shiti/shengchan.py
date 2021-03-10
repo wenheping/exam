375941c3d6bfa21f509a8da2ffa52edbee4b3a51
wen <wen@ob68.wenjing.com>
2021-03-10 16:08:15 +0800

    20210310

--- shiti/shengchan.py
+++ shiti/shengchan.py
@@ -1,2 +1,10 @@
-def get_filename(tmp):
-    return '00000006'
+from django.db import models
+from shiti.models import Shiti
+
+def get_filename(zhangjie):
+    q=Shiti.objects.filter(w_zhishidian=zhangjie)
+    print(q.count())
+    if q.count()>0 :
+      return "00000003"
+    else:
+      return "00000008"
