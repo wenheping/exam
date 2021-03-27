3a800083b0c4d3ec96fe058944391f893bc4275d
wenheping <wenheping2000@hotmail.com>
2021-03-27 20:56:04 +0800

    I shall tag 0.3, now it work to join the test items.

--- shiti/shengchan.py
+++ shiti/shengchan.py
@@ -1,13 +1,28 @@
 from django.db import models
 from shiti.models import Shiti
 
-import fitz
+import fitz,uuid
+
+pdf_path="/root/wen/exam/wenfiles/shiti/2020/"
+
+def w_uuid(string_length=10):
+    random = str(uuid.uuid4())
+    random = random.replace("-","")
+    return random[0:string_length]
 
 def get_filename(zhangjie):
     q=Shiti.objects.filter(w_zhishidian__contains=zhangjie)
+    item_num=q.count()
 
-    if q.count()>0 :
-      return q[0].w_timu
+    if item_num>0 :
+      tmp=fitz.open()
+      for i in range(item_num):
+         doc=fitz.open(pdf_path+q[i].w_timu+".pdf")
+         tmp.insert_pdf(doc)
+         doc.close()
+      file_name=w_uuid()
+      tmp.save(pdf_path+file_name+".pdf")
+      return file_name
     else:
       return "notfound"
 
