7fd09bdc258a459a1ea59cd76a5d3ced95dacbdc
wenheping <wenheping2000@hotmail.com>
2021-03-29 11:26:25 +0800

    Work of 20210329

--- shiti/shengchan.py
+++ shiti/shengchan.py
@@ -17,8 +17,22 @@ def get_filename(zhangjie):
     if item_num>0 :
       tmp=fitz.open()
       for i in range(item_num):
-         doc=fitz.open(pdf_path+q[i].w_timu+".pdf")
+         doc=fitz.open(pdf_path+q[i].w_timu+".pdf") # open origin pdf item
          tmp.insert_pdf(doc)
+         block_lenth=len(tmp[0].getTextBlocks())
+         r1=tmp[0].getTextBlocks()[block_lenth-1][0]
+         r2=tmp[0].getTextBlocks()[block_lenth-1][3] # get the bottom of the text
+
+         pos1=fitz.Rect(80,50,400,500)
+         pos2=fitz.Rect(105,50,400,500)
+         tmp[i].insert_textbox(pos1,"2020",color=(0,0,1))
+         tmp[i].insert_textbox(pos2,q[i].w_shijuan_lei,fontname="china-ss",color=(0,0,1))
+         # add the header to tell which year, which district
+
+         pos3=fitz.Rect(r1,r2+20,595,842)
+         tmp[i].insert_textbox(pos3,"答案：",fontname="china-ss",color=(1,0,0))
+         # add the bottom to tell the key and the explation.
+
          doc.close()
       file_name=w_uuid()
       tmp.save(pdf_path+file_name+".pdf")
