425b4036658fa7f49d7c32efbe871e9483dbd301
wen <wen@ob69.wenjing.com>
2021-08-15 21:23:07 +0800

    work of night on 20210815

--- shiti/views.py
+++ shiti/views.py
@@ -19,9 +19,14 @@ def api(request):
     file_name=''
 
     code = request.POST.get('pdata')
+    test1=request.POST.get('testdata')
+
+    print(test1)
+
     if (code.find('mis')>=0):
       return JsonResponse(data={'output':'click_next'})
     else:
       file_name=shengchan.get_filename(code)
       item_num=shengchan.get_itemnum(code)
-      return JsonResponse(data={'output':file_name,'timushu':item_num})
+      test2="I am" +test1
+      return JsonResponse(data={'output':file_name,'timushu':item_num,"test2":test2})
