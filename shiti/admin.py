f7031224f26c228890848e2baa8cc802d81150b2
wenheping <wenheping2000@hotmail.com>
2022-03-04 14:50:07 +0800

    Update admin.py

--- shiti/admin.py
+++ shiti/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Shiti
-admin.site.register(Shiti)
+
+class ShitiAdmin(admin.ModelAdmin):
+  list_display=('w_id','w_shijuan_lei','w_shiti_lei','w_nian')
+  list_display_links=('w_id','w_shijuan_lei','w_shiti_lei','w_nian')
+
+admin.site.register(Shiti,ShitiAdmin)
